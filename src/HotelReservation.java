3c42c5a3d6f01a63de7ce8d4df44941a03538040
Rajat Gundi <glrajat@gmail.com>
2020-10-20 14:33:02 +0530

    Adding: Ability to Add New Hotels

--- src/HotelReservation.java
+++ src/HotelReservation.java
@@ -1,7 +1,37 @@
+import java.util.*;
+
+class Hotel
+{
+	private String name;
+	private int rewardCustomerRate;
+	private int normalCustomerRate;
+	
+	Hotel(String name, int rewardCustomerRate, int normalCustomerRate){
+		this.name = name;
+		this.normalCustomerRate = normalCustomerRate;
+		this.rewardCustomerRate = rewardCustomerRate;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public int getRewardCustomerRate() {
+		return this.rewardCustomerRate;
+	}
+	
+	public int getNormalCustomerRate() {
+		return this.normalCustomerRate;
+	}
+}
 
 public class HotelReservation {
+	
+	public static List<Hotel> hotels;
+	
 	public static void main(String args[])
 	{
+		hotels = new ArrayList<Hotel>();
 		System.out.println("Welcome to Hotel Reservation Software");
 	}
 
